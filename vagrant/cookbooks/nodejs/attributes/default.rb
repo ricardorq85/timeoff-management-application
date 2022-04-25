ee147360a71ab25e6305117c1028e04ca8bd9e5a
Ricardo Rojas <ricardorq85@gmail.com>
2022-04-24 22:16:06 -0500

    nodejs installation with curl

--- vagrant/cookbooks/nodejs/attributes/default.rb
+++
@@ -1,1 +0,0 @@
-default['nodejs']['package'] = 'nodejs'
--- vagrant/cookbooks/nodejs/recipes/default.rb
+++ vagrant/cookbooks/nodejs/recipes/default.rb
@@ -1,5 +1,9 @@
-apt_update 'Update the apt cache daily' do
-  frequency 86_400
-  action :periodic
+bash 'nodeinstall' do
+  code <<-EOH
+	sudo apt-get install curl 
+	curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - 
+	
+	sudo apt-get install -y nodejs 
+  EOH
+  :run
 end
-package node['nodejs']['package']
